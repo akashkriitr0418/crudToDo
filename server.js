de67c57288a52bc008670fbb2a3bdc04b876cd1b
Akash Kumar <akashkumar@Akashs-Air.mshome.net>
2022-02-14 11:54:24 +0530

    added routers for crud

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors    = require("cors");
 const dotenv  = require('dotenv');
 const bodyParser  = require("body-parser");
 const mongoose    = require("mongoose");
+const crudRoutes  = require("./routes/crud");
 const port        = 3000;
 dotenv.config();
 app  = express();
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 function myinit(){
+    app.use("/api", crudRoutes);
     app.listen(port, () => {
         console.log(`Listening to http://localhost:${port}`);
     });
